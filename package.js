8a67466b6004d943ca802c269337f81ed02b05ec
sjx1207 <1820818189@qq.com>
2025-10-11 15:52:40 +0800

    package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "xiaoshouji",
+  "version": "1.0.0",
+  "description": "小手机项目",
+  "main": "index.js",
+  "scripts": {
+    "build": "echo 'No build required for static site'",
+    "start": "serve ."
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
